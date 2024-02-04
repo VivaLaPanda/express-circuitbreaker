a363adbe589968efc300d47cb7d64080d75dfdbb
VivaLaPanda <adrianrsmith93@gmail.com>
2024-02-04 11:31:12 -0800

    fix: better project config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,41 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: "standard-with-typescript",
-  overrides: [
-    {
-      // Override for JS config files
-      files: ["*.config.js", ".eslintrc.js"],
-      parser: "espree",
-      env: {
-        node: true,
-        browser: false,
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["import"],
+  ignorePatterns: ["dist/"],
+  settings: {
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
       },
-      parserOptions: {
-        ecmaVersion: 2021,
-        sourceType: "script",
+      typescript: {
+        alwaysTryTypes: true,
       },
     },
+  },
+  rules: {
+    "import/no-absolute-path": 2,
+    "import/no-unresolved": 2,
+    "import/no-useless-path-segments": 2,
+    "import/first": 2,
+    "import/no-duplicates": 2,
+    "import/order": 2,
+  },
+  overrides: [
     {
-      env: {
-        node: true,
-      },
-      files: [".eslintrc.{js,cjs}"],
-      parserOptions: {
-        sourceType: "script",
+      files: ["src/**/*.ts"],
+      excludedFiles: ["*.test.ts"],
+      rules: {
+        "no-console": "off",
+        "no-restricted-syntax": [
+          "error",
+          {
+            selector:
+              "CallExpression[callee.object.name='console'][callee.property.name=/^(log|debug|info|trace)$/]",
+            message: "Do not use console.log. Use req.log instead.",
+          },
+        ],
       },
     },
   ],
-  parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-  },
-  rules: {},
 };
